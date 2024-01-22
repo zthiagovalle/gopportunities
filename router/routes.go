a94a7fb822c078d890ed1eedf9c40222b097a263
zthiagovalle <zthiagovalle@gmail.com>
2024-01-21 21:09:08 -0300

    feat: add update opening handler logic

--- router/routes.go
+++ router/routes.go
@@ -2,14 +2,19 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	docs "github.com/zthiagovalle/gopportunities/docs"
 	"github.com/zthiagovalle/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHanlder)
 		v1.POST("/opening", handler.CreateOpeningHanlder)
@@ -17,4 +22,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHanlder)
 		v1.GET("/openings", handler.ListOpeningsHanlder)
 	}
+
+	// Initialize swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
